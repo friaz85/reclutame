db0ce2f61fc62df8d64de975b2f01e1d47dd0337
vtellom <83795474+vtellom@users.noreply.github.com>
2024-09-19 18:28:05 -0600

    VTM: Se cambian paths de las apis

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  url: "https://gfc4159951c9acf-adbreclutamedev.adb.us-ashburn-1.oraclecloudapps.com/ords/workspace_reclutame_dev/apirest/"
+  url: "https://gfc4159951c9acf-adbreclutamedev.adb.us-ashburn-1.oraclecloudapps.com/ords/reclutame/apirest/"
 };
