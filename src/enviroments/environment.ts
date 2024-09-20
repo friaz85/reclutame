307dbd41524a234a8e65461a2811f5ee6eeddcfe
vtellom <83795474+vtellom@users.noreply.github.com>
2024-09-20 10:02:25 -0600

    VTM: Se agrega al environment la ruta de la IA

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   url: "https://gfc4159951c9acf-adbreclutamedev.adb.us-ashburn-1.oraclecloudapps.com/ords/reclutame/apirest/",
-  apiIA: "https://reclutame-proj.uc.r.appspot.com"
+  api:"https://reclutame-proj.uc.r.appspot.com"
 };
